c63133882148f0776b74f37498cfe0f6f82a544b
kosemeci <kosemecialperen37@gmail.com>
2025-02-06 22:08:14 +0300

    created user page styles

--- taskMaster/src/component/User.jsx
+++ taskMaster/src/component/User.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import '../css/user.css'
+
+
 
 function User() {
   return (
-    <div>
-      <h1>User Page</h1>
-      <p>Welcome to the User Page!</p>
+    <div className='user-container'>
+      <div className="user-column column-1">
+        <h2>Ahmet Demir</h2>
+        <div className="user-choose-list">
+          <ul>
+            <li>Bilgilerim</li>
+            <li>Görevlerim</li>
+            <li>Görev Seç</li>
+            <li>Görev Güncelle</li>
+          </ul>
+        </div>
+      </div>
+      <div className="user-column column-2">
+        <p className='user-column-title'>Bilgilerim</p>
+        <div className='user-details'>
+
+        </div>
+      </div>
+
     </div>
   );
 }
