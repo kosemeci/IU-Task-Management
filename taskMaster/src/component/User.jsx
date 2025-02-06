618371abce49e1ddc23789a6c850ddd7b57c8550
kosemeci <kosemecialperen37@gmail.com>
2025-02-06 22:42:03 +0300

    Add state for managing selected title and update logic for active title

--- taskMaster/src/component/User.jsx
+++ taskMaster/src/component/User.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/user.css'
 
 
 
+
 function User() {
+
+  const [firstName, setFirstName] = useState('Ahmet');
+  const [lastName, setLastName] = useState('Demir');
+  const [title, setTitle] = useState('Bilgilerim');
+  const [selectTitle, setSelectTitle] = useState(1);
+
+  useEffect(() => {
+    console.log("Title updated.")
+  }, [title])
+
+  const chanceTitle = (newTitle, index) => {
+    setTitle(newTitle);
+    setSelectTitle(index);
+  }
+
   return (
     <div className='user-container'>
       <div className="user-column column-1">
-        <h2>Ahmet Demir</h2>
+        <h2>{firstName} {" " + lastName}</h2>
         <div className="user-choose-list">
           <ul>
-            <li>Bilgilerim</li>
-            <li>Görevlerim</li>
-            <li>Görev Seç</li>
-            <li>Görev Güncelle</li>
+            <li onClick={() => chanceTitle("Bilgilerim", 1)} className={selectTitle === 1 ? 'selectedTitle' : ''}>Bilgilerim</li>
+            <li onClick={() => chanceTitle("Görevlerim", 2)} className={selectTitle === 2 ? 'selectedTitle' : ''}>Görevlerim</li>
+            <li onClick={() => chanceTitle("Görev Seç", 3)} className={selectTitle === 3 ? 'selectedTitle' : ''}
+            >Görev Seç</li>
+            <li onClick={() => chanceTitle("Görev Güncelle", 4)} className={selectTitle === 4 ? 'selectedTitle' : ''}>Görev Güncelle</li>
           </ul>
         </div>
       </div>
       <div className="user-column column-2">
-        <p className='user-column-title'>Bilgilerim</p>
+        <p className='user-column-title'>{title}</p>
         <div className='user-details'>
-
+          <p style={{ marginTop: 0 }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt repudiandae consequatur consequuntur alias ex iure?</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, error.</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia sunt, asperiores culpa pariatur molestias ipsa veniam aut porro similique laboriosam.</p>
         </div>
       </div>
 
